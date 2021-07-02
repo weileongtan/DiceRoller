f5a5de63c619e49c18a3bbde92a1da9b1474c295
weileongtan <weileongtan@gmail.com>
2021-07-03 01:09:08 +0800

    From the Udacity class

---
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.android.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice rolled", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rand(start: Int, end: Int): Int {
+        require(!(start > end || end - start +1 > Int.MAX_VALUE)) { "Illegal argument" }
+        return Random(System.nanoTime()).nextInt(end - start + 1) + start
+    }
+
+    private fun rollDice() {
+        val randomInt = Random(System.nanoTime()).nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+}
